08c6891465dc7582ee93c1773af7a8e75ae15965
Chris McCormick <chris@mccormick.cx>
2019-07-30 20:02:15 +0800

    Use env vars.

--- redirector.py
+++ redirector.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 # https://stackoverflow.com/a/47084250/2131094
 
-import sys
+import os
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
-if len(sys.argv)-1 != 2:
-    print("""Usage: {} <port_number> <base_url>""".format(sys.argv[0]))
-    sys.exit()
 
 class Redirect(BaseHTTPRequestHandler):
    def do_GET(self):
        self.send_response(302)
-       self.send_header('Location', sys.argv[2] + self.path[1:])
+       self.send_header('Location', os.environ.get("REDIRECT_URL", "") + self.path[1:])
        self.end_headers()
 
 print("Starting redirector HTTPServer.")
-HTTPServer(("", int(sys.argv[1])), Redirect).serve_forever()
+HTTPServer(("", int(os.environ.get("PORT", "8000"))), Redirect).serve_forever()
